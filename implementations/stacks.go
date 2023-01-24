70625109501289c54ccdf368f34bdc0b825ae3ef
abhinash khare <abhinashkhare@abhinashs-MacBook-Pro.local>
2023-01-24 11:54:30 +0530

    stacks added

---
+++ implementations/stacks.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// We can use slice to create stack. Stack mainly contains two operations
+	// push and pop. We can use append and : operator to mimic push and pop.
+	// While popping an element using : operator we need to make sure that it has
+	// atleast 1 element to be popped.
+
+	var intStack = []int{}
+
+	var size_ int
+	fmt.Println("Enter the number of elements ")
+	fmt.Scan(&size_)
+
+	for iter := 0; iter < size_; iter++ {
+		var ele int
+		fmt.Scan(&ele)
+		intStack = append(intStack, ele)
+	}
+
+	fmt.Println("Final Stack after all push operations: ", intStack)
+
+	intStack = intStack[:len(intStack)-1]
+	fmt.Println("Final Stack after a pop operations: ", intStack)
+	intStack = intStack[:len(intStack)-1]
+	fmt.Println("Final Stack after a pop operations: ", intStack)
+	intStack = intStack[:len(intStack)-1]
+	fmt.Println("Final Stack after a pop operations: ", intStack)
+	intStack = intStack[:len(intStack)-1]
+	fmt.Println("Final Stack after a pop operations: ", intStack)
+
+}
